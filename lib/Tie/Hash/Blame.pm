db0514fec2945febdea042bd56325b6143487d35
Rob Hoelz <rob@hoelz.ro>
2012-05-23 21:19:54 +0200

    initial commit

---
+++ lib/Tie/Hash/Blame.pm
@@ -0,0 +1,22 @@
+## no critic (RequireUseStrict)
+package Tie::Hash::Blame;
+
+## use critic (RequireUseStrict)
+use strict;
+use warnings;
+
+1;
+
+__END__
+
+# ABSTRACT: A short description of Tie::Hash::Blame
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=head1 SEE ALSO
+
+=cut
